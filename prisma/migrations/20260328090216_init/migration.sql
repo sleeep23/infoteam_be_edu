35370095759ca90bba4257dc9158adfc5079f698
Dongho Seo <ehcws333@gmail.com>
2026-03-28 18:05:58 +0900

    feat: add prisma package and implement post db using primsa

---
+++ prisma/migrations/20260328090216_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
