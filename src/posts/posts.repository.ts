e467954f3b7f19dc8a2a44c9747f30a57c5197af
Dongho Seo <ehcws333@gmail.com>
2026-03-31 17:35:19 +0900

    chore: add return types for posts repository

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -1,41 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../lib/prisma/src/prisma.service';
 import { CreatePostDto, UpdatePostDto } from 'src/posts/dto';
+import { Post } from './entities/post.entity';
 
 @Injectable()
 export class PostsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
+  findAll(): Promise<Post[]> {
     return this.prisma.post.findMany();
   }
 
-  findById(id: number) {
+  findById(id: number): Promise<Post | null> {
     return this.prisma.post.findUnique({
       where: { id },
     });
   }
 
-  findByUserId(userId: number) {
+  findByUserId(userId: number): Promise<Post[]> {
     return this.prisma.post.findMany({
       where: { userId },
     });
   }
 
-  create(dto: CreatePostDto) {
+  create(dto: CreatePostDto): Promise<Post> {
     return this.prisma.post.create({
       data: dto,
     });
   }
 
-  update(id: number, dto: UpdatePostDto) {
+  update(id: number, dto: UpdatePostDto): Promise<Post> {
     return this.prisma.post.update({
       where: { id },
       data: dto,
     });
   }
 
-  delete(id: number) {
+  delete(id: number): Promise<Post> {
     return this.prisma.post.delete({
       where: { id },
     });
